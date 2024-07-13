b66946efbca341a2c4ad0421ad592216669e3971
Subhajit <dass8684@gmail.com>
2024-07-13 19:09:43 +0530

    add js

---
+++ script.js
@@ -0,0 +1,8 @@
+let colorWhite= document.getElementsByTagName("span");
+
+
+for (let index = 0; index < colorWhite.length; index++) {
+        colorWhite[index].style.color="white";
+        console.log(index);
+}
+
